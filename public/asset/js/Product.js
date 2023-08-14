9654c9331a0dadea9c62e3b82d5e4e885ebfe7b3
Nguyễn Văn Quang <quangnvgch211372@fpt.edu.vn>
2023-08-14 21:33:45 +0700

    hoàn thành phần product detail

--- public/asset/js/Product.js
+++ public/asset/js/Product.js
@@ -41,30 +41,28 @@ function showProducts1() {
 
             data.forEach(element => {
                 output += `
-            <tr id="productRow_${element.id}" style="border:1.5px solid rgb(67, 187, 185); font-weight: bold; width: 1000px;">
-            <td style="width: 14%;">${element.name}</td>
-            <td>${element.description}</td>
-            <td>${element.price}</td>
-            <td>
-            <div class="image-container">
-                <img src="http://127.0.0.1:8000/asset/img/${element.image}" class="img-fluid" alt="">
-            </div>
-        </td>
-
-            <td>${element.name1}</td>
-            <td class="text-center">
-                <div class="d-flex justify-content-center">
-                <a href="/updateProduct/${element.id}" class="btn btn-warning btn-sm font-weight-bold">Edit</a>
-
-
+                <tr id="productRow_${element.id}" style="border:1.5px solid rgb(67, 187, 185); font-weight: bold;">
+                <td style="width: 14%;">${element.name}</td>
+                <td>${element.description}</td>
+                <td>${element.price}</td>
+                <td>
+                <div class="image-container" style = ""width:182px>
+                    <img src="http://127.0.0.1:8000/asset/img/${element.image}" class="img-fluid" alt="" style="width: 100%;">
                 </div>
             </td>
-            <td class="bg-light border text-center">
-                <div class="d-flex justify-content-center">
-                    <button class="btn btn-danger btn-sm font-weight-bold" onclick="deleteProduct(${element.id})">Delete</button>
-                </div>
-            </td>
-        </tr>`;
+
+                <td>${element.name1}</td>
+                <td class="text-center">
+                    <div class="d-flex justify-content-center">
+                        <a href="/updateProduct/${element.id}" class="btn btn-warning btn-sm font-weight-bold">Edit</a>
+                    </div>
+                </td>
+                <td class="bg-light border text-center">
+                    <div class="d-flex justify-content-center">
+                        <button class="btn btn-danger btn-sm font-weight-bold" onclick="deleteProduct(${element.id})">Delete</button>
+                    </div>
+                </td>
+            </tr>`;
             });
             listContainer.innerHTML = output;
         })
